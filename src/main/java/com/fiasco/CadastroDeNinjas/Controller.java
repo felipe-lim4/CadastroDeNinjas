df94470f91836967495b52ef8eeae02f7fc6c273
Felipe Lima Vieira <felipe.vieira@sptech.school>
2025-08-15 21:34:07 -0300

    aula 2

---
+++ src/main/java/com/fiasco/CadastroDeNinjas/Controller.java
@@ -0,0 +1,17 @@
+package com.fiasco.CadastroDeNinjas;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Essa é a minha primeira mensagem nesta rota";
+    }
+
+}
